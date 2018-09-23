f03d7dfb94e22b3dd261c9345c43a589811beb56
Hector <hector.jimenez.sanchez@gmail.com>
2018-09-23 23:32:20 +0100

    fix heroku

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 // server.js
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
 const app = express();
+
+
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+console.log('DIRECTION: ', __dirname+'\\dist')
+app.use(express.static(__dirname + '\\dist'));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname));
+});
+
+const port = process.env.PORT || 8080;
+app.set('port',port);
+
+const server = http.createServer(app);
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+server.listen(port, ()=> console.log(`Running`));
