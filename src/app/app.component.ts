8388dcef14a1aab6cc3a88af93a55f330ce9d8a0
Hector <hector.jimenez.sanchez@gmail.com>
2018-10-09 21:41:17 +0100

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { AppNavigationService } from './core/services/app-navigation/app-navigat
 export class AppComponent implements OnInit {
 
   public nextPage: string;
-  title = 'Welcome!';
+  title = 'PLAYING WITH ANGULAR';
 
   constructor(public router: Router,
   public appNavigationService: AppNavigationService) {
