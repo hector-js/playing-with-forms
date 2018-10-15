36a17a940f7f12c5ffad79b616bd41ecb5b104e4
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2018-10-15 20:10:43 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { AppNavigationService } from './core/services/app-navigation/app-navigat
 export class AppComponent implements OnInit {
 
   public nextPage: string;
-  title = 'Hola CARMEN y MANOLO';
+  title = 'Hola ARDILLA';
 
   constructor(public router: Router,
   public appNavigationService: AppNavigationService) {
